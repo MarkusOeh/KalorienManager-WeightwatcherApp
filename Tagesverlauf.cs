f892c6dcd5bec630e55b7049315fe041d631d2f1
Markus Mustermann <dontspammarkus@googlemail.com>
2024-07-15 15:59:10 +0200

    Liste Tagesverlauf testhalber ausgefüllt

--- Tagesverlauf.cs
+++ Tagesverlauf.cs
@@ -12,8 +12,18 @@ namespace KalorienManager_WeightwatcherApp
         {
 
             Console.Clear();
-            Console.WriteLine("-----TAGESVERLAUF-----");
-            
+            Console.WriteLine("\t-----TAGESVERLAUF-----\n");
+            Console.WriteLine("\n\n\t MENGE\t\tNAHRUNGSMITTEL\t\tKCAL");
+            Console.WriteLine("\t 200g\t\tTesteintrag\t\t500");
+            Console.WriteLine("\t 1Stk\t\tTestkuchen\t\t600");
+            Console.WriteLine("\t 1 Portion\t\tTestessen\t\t1000");
+            Console.WriteLine("\n\n\t SUMME: XXX KALORIEN");
+            Console.WriteLine("\n\t DU DARFST NOCH SUMME minus TAGESLIMIT KONSUMIEREN\n\n");
+            Console.WriteLine("\t (N)EUER EINTRAG");
+            Console.WriteLine("\t (L)ETZTEN EINTRAG LÖSCHEN");
+            Console.WriteLine("\t (Z)U VORLAGENLISTE SPRINGEN");
+            Console.ReadKey();
+
         }
     }
 }

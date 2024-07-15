76c5ac3dc76941e473155dfcc77ec8546495164a
Markus Mustermann <dontspammarkus@googlemail.com>
2024-07-15 13:23:16 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KalorienManager_WeightwatcherApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

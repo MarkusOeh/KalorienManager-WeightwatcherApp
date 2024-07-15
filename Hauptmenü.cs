9090190e9be01b40b9d3f6085867fcec16761c64
Markus Mustermann <dontspammarkus@googlemail.com>
2024-07-15 13:42:27 +0200

    Hauptmenü und Untermenüs erstellt

---
+++ Hauptmenü.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalorienManager_WeightwatcherApp
+{
+    internal class Hauptmenü
+    {
+    }
+}
---
+++ Kalenderwochen.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalorienManager_WeightwatcherApp
+{
+    internal class Kalenderwochen
+    {
+    }
+}

8d538cc6fffc6775f64056fb8af3bdac332395de
Markus Mustermann <dontspammarkus@googlemail.com>
2024-07-15 15:02:40 +0200

    Klasse Hauptmenü gelöscht und das Hauptmenü in Program.cs integriert

--- Program.cs
+++ Program.cs
@@ -15,9 +15,18 @@
 
             Console.ForegroundColor = ConsoleColor.Green;
             welcome.welcomeScreen();
-            hauptmenü.hauptMenü();
-            limitfestlegen.limitFestlegen();
-            hauptmenü.hauptMenü();
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine("\t-----HAUPTMENÜ-----");
+            Console.WriteLine("");
+            Console.WriteLine("\tTAGESVERLAUF");
+            Console.WriteLine("\tVORLAGENSEITE");
+            Console.WriteLine("\tGESAMTVERLAUF");
+            Console.WriteLine("\tKALENDERWOCHEN");
+            Console.WriteLine("\tTUTORIAL");
+            Console.WriteLine("\n\tLIMIT FESTLEGEN");
+            Console.ReadKey();
+
             Console.ReadKey();
 
         }

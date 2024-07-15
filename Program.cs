9219acbefb38bb64769a7fb3a0548889dd96805f
Markus Mustermann <dontspammarkus@googlemail.com>
2024-07-15 13:38:15 +0200

    Projekt erstellt

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            WelcomeScreen welcome = new WelcomeScreen();
+            welcome.welcomeScreen();
         }
     }
 }
---
+++ WelcomeScreen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalorienManager_WeightwatcherApp
+{
+    public class WelcomeScreen
+    {
+        public void welcomeScreen()
+        {
+            Console.WriteLine("Willkommen bei KalorienManager App");
+            Console.ReadKey();
+        }
+    }
+}
